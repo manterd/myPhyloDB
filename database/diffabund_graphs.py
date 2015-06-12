66a8a909cb6c76ae5f85b80b32b7ee7e7b9916b7
Korsam <Motorman24@gmail.com>
2015-06-12 17:14:58 -0600

    Improved DiffAbund Binomial test output, started on graphing

--- database/diffabund_graphs.py
+++ database/diffabund_graphs.py
@@ -12,6 +12,7 @@ import numpy as np
 import datetime
 from pyper import *
 import math
+from models import Kingdom, Phyla, Class, Order, Family, Genus, Species
 
 
 stage = ''
@@ -102,7 +103,7 @@ def getDiffAbund(request):
         r.assign("countTable", taxaDF)
         r("cds <- newCountDataSet(countTable, condition)")
         r("cds <- estimateSizeFactors(cds)")
-        r("cds <- estimateDispersions(cds, method='blind', fitType='local')")
+        r("cds <- estimateDispersions(cds, method='blind', fitType='local', sharingMode='fit-only')")
 
         pycds = r.get("sizeFactors(cds)")
         found = 0
@@ -118,7 +119,7 @@ def getDiffAbund(request):
                 sizeFactor.append(1)
             r.assign("sizeFactor", sizeFactor)
             r("cds$sizeFactor <- sizeFactor")
-            r("cds <- estimateDispersions(cds, method='blind', fitType='local')")
+            r("cds <- estimateDispersions(cds, method='blind', fitType='local', sharingMode='fit-only')")
 
         if NormMeth == 1 and DESeq_error == 'no':
             result += 'Data were normalized by DESeq...\n'
@@ -130,7 +131,7 @@ def getDiffAbund(request):
 
         stage = 'Step 2 of 4: Normalizing data...complete'
         stage = 'Step 3 of 4: Performing statistical test...'
-
+        AxisArray = {}
         # nbinomTest
         if StatTest == 1:
             mergeList = metaDF['merge'].tolist()
@@ -145,18 +146,49 @@ def getDiffAbund(request):
                         r.assign("trt1", mergeSet[i])
                         r.assign("trt2", mergeSet[j])
                         r("res <- nbinomTest(cds, trt1, trt2)")
-
                         nbinom_res = r.get("res")
-                        result += str(nbinom_res)
-                result += '===============================================\n\n\n'
 
-                # TODO modify output sent to result string
-                    # write nbinom_res to datafame
-                    # add taxa name and rank columns
-                    # remove baseMean
-                    # output to string
+                        names = []
+                        for item in nbinom_res["id"]:
+                            names.append(findTaxa(item))
 
-                # TODO output means to datatable
+                        try:
+                            nbinom_res['Taxa name'] = names
+                            nbinom_res.rename(columns={'id': 'Taxa ID'}, inplace=True)
+                            stuff = ['Taxa ID', 'Taxa name', ' baseMean ', ' baseMeanA ', ' baseMeanB ', ' foldChange ', ' log2FoldChange ', ' pval ', ' padj ']
+                            nbinom_res = nbinom_res.reindex(columns=stuff)
+                            nbinom_res.rename(columns={' log2FoldChange ': 'log2FoldChange'}, inplace=True)
+                            nbinom_res.rename(columns={' baseMeanA ': 'baseMeanA'}, inplace=True)
+                            nbinom_res.rename(columns={' baseMeanB ': 'baseMeanB'}, inplace=True)
+                            nbinom_res.rename(columns={' pval ': 'pval'}, inplace=True)
+                            nbinom_res.rename(columns={' padj ': 'padj'}, inplace=True)
+                        except:
+                            print ("Join failed")
+
+                        try:
+                            iterationName = str(mergeSet[i]) + ' vs ' + str(mergeSet[j])  # + taxa
+                            print ("Iteration: "+iterationName)
+                            dataSet = [None]
+                            print "DataSet: "+str(dataSet)
+                            for thing in nbinom_res["log2FoldChange"]:
+                                print("Element!")
+                                print("thing: "+str(thing))
+                                dataSet.append(thing)
+                            AxisArray[iterationName] = dataSet
+                        except:
+                            print("Failed to add xAxis data")
+
+                        try:
+                            nbinom_res.drop(' baseMean ', axis=1, inplace=True)
+
+                        except:
+                            print"Deletion failed"
+                            print sys.exc_info()[0]
+
+                        result += nbinom_res.to_string()
+                        result += '\n===============================================\n\n\n'
+
+                # TODO output means to datatable ???
                     # write
 
 
@@ -169,9 +201,76 @@ def getDiffAbund(request):
             # x = taxa name
             # y = log2 fold change
             # significant points in red
-            # OR see if we can a
+
+        '''regrDict = {}
+        regrDict['type'] = 'line'
+        regrDict['name'] = 'R2: ' + str(r_square) + '; p-value: ' + str(p_value) + '<br>' + '(y = ' + str(slope) + 'x' + ' + ' + str(intercept) + ')'
+        regrDict['data'] = regrList
+        seriesList.append(regrDict)'''
+
+        xAxisDict = {}
+        yAxisDict = {}
+
+        xTitle = {}
+        xTitle['text'] = taxaDF  # taxalist here?
+        xAxisDict['title'] = xTitle
+
+        yTitle = {}
+        yTitle['text'] = fieldList[0]
+        yAxisDict['title'] = yTitle
+
+        finalDict['series'] = seriesList
+        finalDict['xAxis'] = xAxisDict
+        finalDict['yAxis'] = yAxisDict
 
         stage = 'Step 4 of 4: Preparing graph data...completed'
         finalDict['text'] = result
         res = simplejson.dumps(finalDict)
         return HttpResponse(res, content_type='application/json')
+
+def findTaxa(id):
+    taxa = ""
+    try:
+        temp = Kingdom.objects.filter(kingdomid=id)
+        taxa += temp[0].kingdomName
+    except:
+        # not kingdom, try next one
+        try:
+            temp = Phyla.objects.filter(phylaid=id)
+            taxa += temp[0].phylaName
+        except:
+            try:
+                temp = Class.objects.filter(classid=id)
+                taxa += temp[0].className
+            except:
+                try:
+                    temp = Order.objects.filter(orderid=id)
+                    taxa += temp[0].orderName
+                except:
+                    try:
+                        temp = Family.objects.filter(familyid=id)
+                        taxa += temp[0].familyName
+                    except:
+                        try:
+                            temp = Genus.objects.filter(genusid=id)
+                            taxa += temp[0].genusName
+                        except:
+                            try:
+                                temp = Species.objects.filter(speciesid=id)
+                                taxa += temp[0].speciesName
+                            except:
+                                # not found, error!
+                                print("Could not find taxa for "+str(id))
+    return taxa
+
+    '''qs1 = Species.objects.values('kingdomid__kingdomName', 'kingdomid', 'phylaid__phylaName', 'phylaid', 'classid__className', 'classid', 'orderid__orderName', 'orderid', 'familyid__familyName', 'familyid', 'genusid__genusName', 'genusid', 'speciesName', 'speciesid')
+    speciesDF = pd.DataFrame.from_records(qs1)
+    speciesDF.rename(columns={'kingdomid__kingdomName': 'Kingdom Name', 'kingdomid': 'Kingdom ID'}, inplace=True)
+    speciesDF.rename(columns={'phylaid__phylaName': 'Phylum Name', 'phylaid': 'Phylum ID'}, inplace=True)
+    speciesDF.rename(columns={'classid__className': 'Class Name', 'classid': 'Class ID'}, inplace=True)
+    speciesDF.rename(columns={'orderid__orderName': 'Order Name', 'orderid': 'Order ID'}, inplace=True)
+    speciesDF.rename(columns={'familyid__familyName': 'Family Name', 'familyid': 'Family ID'}, inplace=True)
+    speciesDF.rename(columns={'genusid__genusName': 'Genus Name', 'genusid': 'Genus ID'}, inplace=True)
+    speciesDF.rename(columns={'speciesName': 'Species Name', 'speciesid': 'Species ID'}, inplace=True)
+    table = speciesDF.to_html(classes="table display", columns=['Kingdom Name', 'Kingdom ID', 'Phylum Name', 'Phylum ID', 'Class Name', 'Class ID', 'Order Name', 'Order ID', 'Family Name', 'Family ID', 'Genus Name', 'Genus ID', 'Species Name', 'Species ID'])
+    table = table.replace('border="1"', 'border="0"')'''
