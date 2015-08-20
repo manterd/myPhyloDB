7c67a93f47edd7d017977fc57f9bf443a3a7c628
manterd <daniel.manter72@gmail.com>
2015-08-20 17:26:21 -0600

    linux install.sh updated

--- Installers/Linux/install.sh
+++ Installers/Linux/install.sh
@@ -1,26 +1,41 @@
-#! /usr/bin/sh
+#! usr/bin/sh
 
-echo "Checking if a current version of myPhyloDB exists..."
+echo "Installing myPhyloDB vers. 1.0\n"
+echo "Checking if myPhyloDB exists...\n"
 
-if [-d $HOME/myPhyloDB]
+if [ -d "$HOME/myPhyloDB" ]
     then
+	echo "A previous installation of myPhyloDB was detected..."
         echo -n "Would you like to keep your current database files [y/n]?"
         read response
     else
-        response=y
+	echo "No previous installations of myPhyloDB were detected..."
+        response=n
 fi
 
-echo response
-
-cp $HOME/myPhyloDB/dbMicrobe $HOME/myPhyloDB_temp/dbMicrobe
-cp -r $HOME/myPhyloDB/uploads $HOME/myPhyloDB_temp/uploads
-tar -zxf myPhyloDB.tar.gz -C $HOME
-mv ./dbMicrobe $HOME/dbMicrobe
-mv ./uploads $HOME/uploads
+if [ $response = y ]
+    then
+	mkdir $HOME/myPhyloDB_temp
+	cp $HOME/myPhyloDB/dbMicrobe $HOME/myPhyloDB_temp/dbMicrobe
+	cp -r $HOME/myPhyloDB/uploads $HOME/myPhyloDB_temp/uploads
+	tar -zxf myPhyloDB.tar.gz -C $HOME
+	rm $HOME/myPhyloDB/dbMicrobe
+	rm -rf $HOME/myPhyloDB/uploads
+	cp $HOME/myPhyloDB_temp/dbMicrobe $HOME/myPhyloDB/dbMicrobe
+	cp -r $HOME/myPhyloDB_temp/uploads $HOME/myPhyloDB/uploads
+	rm -rf $HOME/myPhyloDB_temp
+    else
+	if [ -d "$HOME/myPhyloDB" ]
+	    then
+	        rm -rf $HOME/myPhyloDB	
+	fi
+	tar -zxf myPhyloDB.tar.gz -C $HOME
+fi
 
 mkdir -p $HOME/.icons
 cp $HOME/myPhyloDB/media/images/database_2_48.png $HOME/.icons/database_2_48.png
 cp myPhyloDB.desktop $HOME/Desktop/myPhyloDB.desktop
 chmod +x $HOME/Desktop/myPhyloDB.desktop
 
-echo Done!
+echo ""
+echo "myPhyloDB installation is finished!"
