35b9ff046a070eed2b80e52d32ececa0cd79475a
manterd <daniel.manter@ars.usda.gov>
2015-09-09 10:32:59 -0600

    Adding Illumina support.  Multifile upload for fastaq files.

--- database/utils.py
+++ database/utils.py
@@ -162,4 +162,4 @@ class MultiFileField(forms.FileField):
             raise ValidationError(self.error_messages['max_num'] % {'max_num': self.max_num, 'num_files': num_files})
         for uploaded_file in data:
             if uploaded_file.size > self.maximum_file_size:
-                raise ValidationError(self.error_messages['file_size'] % { 'uploaded_file_name': uploaded_file.name})
+                raise ValidationError(self.error_messages['file_size'] % {'uploaded_file_name': uploaded_file.name})
