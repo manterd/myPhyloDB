dac48d209d8af57a0ff64b484b8e8c873d562528
manterd <daniel.manter@ars.usda.gov>
2016-02-02 22:41:21 -0700

    Linux v.1.1.2 complete

--- build_installers/Windows/make.bat
+++ build_installers/Windows/make.bat
@@ -1,5 +1,12 @@
-rem This file will not install myPhyloDB.
-rem To install, please download the installer at: http://www.ars.usda.gov/services/software/download.htm?softwareid=472
+rem This file is used to make a new myPhyloDB installer
+rem it will not install myPhyloDB
+
+rem If you are looking for a pre-built installer
+rem Please download the installer at: http://www.ars.usda.gov/services/software/download.htm?softwareid=472
+
+rem To run this file type the following in your terminal
+rem cd C:\Users\daniel.manter.\Documents\GitHub\myPhyloDB
+rem make.bat
 
 cd C:\Users\daniel.manter.\Documents\GitHub\myPhyloDB
 pyinstaller -D serve-win.spec
