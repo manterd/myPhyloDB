c339663e49d3a202dfc37b1d2e50bbc58805ac3a
manterd <daniel.manter@ars.usda.gov>
2016-05-23 08:41:29 -0600

    Fix thread id error on stop analysis.

--- database/utils.py
+++ database/utils.py
@@ -176,30 +176,25 @@ class MultiFileField(forms.FileField):
 
 
 def _async_raise(tid, exctype):
-    """raises the exception, performs cleanup if needed"""
     if not inspect.isclass(exctype):
         raise TypeError("Only types can be raised (not instances)")
-    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(exctype))
+    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(tid), ctypes.py_object(exctype))
+
     if res == 0:
         raise ValueError("invalid thread id")
     elif res != 1:
-        # """if it returns a number greater than one, you're in trouble,
-        # and you should call it again with exc=NULL to revert the effect"""
         ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, 0)
         raise SystemError("PyThreadState_SetAsyncExc failed")
 
 
 class stoppableThread(threading.Thread):
     def _get_my_tid(self):
-        """determines this (self's) thread id"""
         if not self.isAlive():
             raise threading.ThreadError("the thread is not active")
 
-        # do we have it cached?
         if hasattr(self, "_thread_id"):
             return self._thread_id
 
-        # no, look for it in the _active dict
         for tid, tobj in threading._active.items():
             if tobj is self:
                 self._thread_id = tid
@@ -208,19 +203,13 @@ class stoppableThread(threading.Thread):
         raise AssertionError("could not determine the thread's id")
 
     def raise_exc(self, exctype):
-        """raises the given exception type in the context of this thread"""
         _async_raise(self._get_my_tid(), exctype)
 
     def terminate(self):
-        """raises SystemExit in the context of the given thread, which should
-        cause the thread to exit silently (unless caught)"""
-        self.raise_exc(BaseException)
+        self.raise_exc(SystemExit)
 
 
 def remove(path):
-    """
-    Remove the file or directory
-    """
     if os.path.isdir(path):
         try:
             os.rmdir(path)
