24bb61d52f8399b86e7cdfd08a62dea7a1a328b2
manterd <daniel.manter@ars.usda.gov>
2016-06-07 13:57:46 -0600

    Fixed status overwriting data, confirmed 5 max JS threads

--- database/anova/anova_graphs.py
+++ database/anova/anova_graphs.py
@@ -48,26 +48,26 @@ def removeRIDANOVA(RID):
 
 def statusANOVA(request):
     global base, stage, time1, time2, TimeDiff
-    if request.is_ajax():
-        RID = request.GET["all"]
-        time2[RID] = time.time()
-        try:
-            TimeDiff[RID] = time2[RID] - time1[RID]
-        except:
-            TimeDiff[RID] = 0
-        try:
-            if TimeDiff[RID] == 0:
-                stage[RID] = 'Analysis has been placed in queue, there are '+str(database.queue.q.qsize())+' others in front of you.'
-            else:
-                stage[RID] = str(base[RID]) + '<br>Analysis has been running for %.1f seconds' % TimeDiff[RID]
-        except:
-            if TimeDiff[RID] == 0:
-                stage[RID] = 'In queue'
-            else:
-                stage[RID] = '<br>Analysis has been running for %.1f seconds' % TimeDiff[RID]
-        myDict = {'stage': stage[RID]}
-        json_data = simplejson.dumps(myDict, encoding="Latin-1")
-        return HttpResponse(json_data, content_type='application/json')
+    # if request.is_ajax():
+    RID = request.GET["all"]
+    time2[RID] = time.time()
+    try:
+        TimeDiff[RID] = time2[RID] - time1[RID]
+    except:
+        TimeDiff[RID] = 0
+    try:
+        if TimeDiff[RID] == 0:
+            stage[RID] = 'Analysis has been placed in queue, there are '+str(database.queue.stat(RID))+' others in front of you.'
+        else:
+            stage[RID] = str(base[RID]) + '<br>Analysis has been running for %.1f seconds' % TimeDiff[RID]
+    except:
+        if TimeDiff[RID] == 0:
+            stage[RID] = 'In queue'
+        else:
+            stage[RID] = '<br>Analysis has been running for %.1f seconds' % TimeDiff[RID]
+    myDict = {'stage': stage[RID]}
+    json_data = simplejson.dumps(myDict, encoding="Latin-1")
+    return HttpResponse(json_data, content_type='application/json')
 
 
 def getCatUnivData(request, RID, stopList, PID):
