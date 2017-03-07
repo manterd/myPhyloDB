2db1f69f3e87af6a4223aa221b320d824a28a640
manterd <daniel.manter@ars.usda.gov>
2017-03-07 13:28:52 -0700

    removed extra line from grouped-category

--- functions/analysis/anova_graphs.py
+++ functions/analysis/anova_graphs.py
@@ -450,8 +450,7 @@ def getCatUnivData(request, RID, stops, PID):
                         g2indexvals = grouped2.index.values
                         level = g2indexvals[0].__len__()
                         labelTree = recLabels(g2indexvals, level)
-                        labelTree.pop('name')
-                        xAxisDict['categories'] = [labelTree]
+                        xAxisDict['categories'] = labelTree['categories']
 
                     # /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\//\ #
                     if stops[PID] == RID:
