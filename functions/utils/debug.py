a40f455f2961fe9d3dbe36bb9a697aec962c0b7d
Korsam <motorman24@gmail.com>
2019-10-11 17:57:12 -0600

    File chunk uploader - working (quite slow on large files)

--- functions/utils/debug.py
+++ functions/utils/debug.py
@@ -2,11 +2,11 @@
 #   in theory this is used by all files, including ones utils_df imports
 
 flag = True    # only need to switch this one variable for the whole code to go into verbose mode
-# TODO 1.4 Admin account auto-debug, switch back to request-linked debug flags.
-# TODO 1.4 Error log saves original request for admin to rerun later (with admin auto debug)
+
+# TODO 1.4 Error log saves original request for admin to rerun later
 
 def debug(*msg):    # *msg allows any number of arguments to be passed in, since we just want to print everything
     # print msg if debug flag is set to true
     if flag:
-        print msg
+        print msg   # how to clean up these messages...
     # would be somewhat convenient to check memory usage here
--- functions/utils/file_handler.py
+++ functions/utils/file_handler.py
@@ -1,14 +1,22 @@
-from database.views import files
 import json
 from django.http import HttpResponse
 import hashlib
-import sys
+import os
+import math
+import datetime
+from database.views import subDirList
+from functions.utils.debug import debug
 
 chunkStorageDict = {}
 # chunkStorageDict uses key RID with value being a list, since we know which order we should be getting these in per RID
+chunkSize = 10000  # 10 KB chunk max, make sure this is synced with files.html value
+maxMemory = 10000000  # 10 MB max before offloading to temp file
+maximumMemorizedChunks = maxMemory / chunkSize
+
+# TODO 1.3 need a process that cleans up requests which haven't finished after a period of time with no updating
+
 
 def fileUploadChunk(request):
-    #print "fileUploadChunk"
     global chunkStorageDict
     retDict = {"error": "none"}
     # given an RID and a chunk of a file, write chunk to temp file under directory labelled by RID, name chunk by sequence
@@ -16,18 +24,19 @@ def fileUploadChunk(request):
     # we want to limit upload files and how many uploads can occur at a time (ideally just one at a time per user)
     try:
         #myToken = request.POST['csrfmiddlewaretoken']
-        # TODO 1.3 put user check here, don't even start this if the user isn't logged in or the token is not valid
-        #print "Trying"
+
+        if not request.user.is_authenticated():
+            # TODO 1.3 security flag
+            raise Exception("Error: invalid user credentials")
+
         myData = json.loads(request.body.rsplit('&', 1)[0])
         myRID = myData['RID']
         myChunkNum = myData['chunkNum']
-        # TODO 1.3 need to verify chunk is at or below expected size, also some sort of frequency control (security!)
-        # TODO 1.3 file size control, write to file when enough chunks are in memory, reset memory. Rebuild final file from temp files + current memory
-        myChunk = myData['chunk']   # in theory knowing if this is a string type file or not could fix reassembly
-        #myChunk = request.POST['chunkBinaryData']
+        # TODO 1.3 Frequency control (security!)
+        myChunk = myData['chunk']
         # need to hash the chunk and return the hash
-        #print "Encoding and hashing", myChunk
-        # at this point, myChunk is an Int32 array made into a dictionary, how to convert back to binary (or hex should work too)
+        # at this point, myChunk is a Uint8 array made into a dictionary, convert back to array to make hash, then
+        # send it back to binary entirely for the chunk saving / file writing later
         chunkData = []
         myKeys = myChunk.keys()
         keyList = []
@@ -35,132 +44,142 @@ def fileUploadChunk(request):
             keyList.append(int(k))
         keyList = sorted(keyList)
         for index in keyList:
-            chunkData.append(myChunk[str(index)])  # have to string case since its not actuall an index
-
+            chunkData.append(myChunk[str(index)])  # have to string case since its not actually an index
 
         myBuffer = bytearray(chunkData)
-        #print "Buffer:", myBuffer
         myHash = hashlib.md5(myBuffer).hexdigest()
-        #print "Hash:", myHash
         retDict['hash'] = myHash
         # now actually store the data somewhere, either write to temp file or hoard the whole thing in memory (might be huge)
         # save in memory up to a certain amount, then write that chunk down as a temp file. Cleanup during uploadComplete
         if myRID not in chunkStorageDict.keys():
             chunkStorageDict[myRID] = []
-            print "New file being uploaded"
+            debug(request.user.username, "started an upload!")
         myStorage = chunkStorageDict[myRID]
 
         # verify this chunk fits size specification  # TODO 1.3 implement
+        if len(myBuffer) > chunkSize:
+            raise Exception("Error: Received too much data")
 
-        #myChunk = myChunk.decode('utf-8')
-        #myChunk = bytes(myChunk)
-        #myChunk = myChunk.encode('utf-8')
-        #myChunk = "".join(myChunk).decode('utf-8')
-
-        #myChunk = myChunk.encode("ASCII")
-
-        #print myChunk
-
-        if myChunkNum < len(myStorage):
+        if myChunkNum % maximumMemorizedChunks < len(myStorage):  # by the time we've reached max, we can assume reset
             # we have a duplicate chunk send, assume the hash mismatched from before and overwrite
             # BUT we also should keep track of how many times a duplicate runs through, limit 10
-            print "DUPLICATE CHUNK"  # TODO 1.3 implement tracker
+            print "DUPLICATE CHUNK"  # TODO 1.3 implement tracker, dupes suggest hash mismatch or weird DoS attack
             myStorage[myChunkNum] = myBuffer
         else:
             # normal case where this chunk is new
-            #print "Saving chunk"
-            #print myChunk
             myStorage.append(myBuffer)
-            #print "saved chunk"
 
-        # TODO 1.3 check myStorage is below max bunch size (maximum number of chunks before writing to disk)
-        # if above max, write bunch down as a temp file labelled RID-A.bunch or somesuch
-        #print "placing into storage"
+            # memory to temp file handler
+            if len(myStorage) == maximumMemorizedChunks:
+                # bunch starts sounding like a weird word the more I see it here, refactor? TODO 1.3 but really though
+                # if above max, write bunch down as a temp file labelled RID-A.bunch or somesuch
+                bunchPath = "temp/bunches/"+myRID
+                bunchNumber = int(math.floor(myChunkNum / maximumMemorizedChunks))  # how many times have we done this before for this RID?
+                if not os.path.exists(bunchPath):
+                    try:
+                        os.makedirs(bunchPath)
+                    except Exception as errr:
+                        raise Exception("Error with making new bunch directory:"+str(errr))
+                if writeBunchFile(myStorage, bunchPath, bunchNumber):
+                    # file wrote successfully
+                    # empty old list from memory after writing file
+                    del myStorage[:]
+                    myStorage = []
+                else:
+                    raise Exception("Error with bunch writer")
+
         chunkStorageDict[myRID] = myStorage
 
     except Exception as e:
-        print e
+        print e  # TODO 1.3 log this
         retDict['error'] = e
     res = json.dumps(retDict)
-    #print "returning", HttpResponse(res, content_type='json')
     return HttpResponse(res, content_type='json')
 
 
+def writeBunchFile(chunks, path, number):
+    if not os.path.exists(path):
+        try:
+            os.makedirs(path)
+        except Exception as errr:
+            print "Error with making new bunch directory:", errr
+            return False
+    bunchFilePath = path + "/" + str(number)
+    with open(bunchFilePath, "w+b") as bunchFile:
+        for chunk in chunks:
+            bunchFile.write(chunk)
+        bunchFile.close()
+        return True
+    return False
+
+
 def fileUploadComplete(request):
-    print "Called fileUploadComplete"
-    # myToken = request.POST['csrfmiddlewaretoken']
+    # myToken = request.POST['csrfmiddlewaretoken'] TODO 1.3 are we supposed to check these or does Django on its own?
     myData = json.loads(request.body.split('&')[0])
     myType = myData['type']
     myName = myData['name']
     myRID = myData['RID']
     retDict = {"error": "none"}
+
     try:
-        # This function needs to take an RID and find the temp folder holding chunks of a file, put them all back together
-        # Use the old fileUpFunc code to get the right location to store the final file
-        # TODO 1.3 Need to check for .bunch files with RID at start of name
-        # Get currently loaded chunks from memory
-        myChunkData = chunkStorageDict[myRID]
-        myFileData = ""
-        # TODO 1.3 holy cow we need to run the filename through some checks before doing this
-        with open(myName, "w+b") as myFile:
-            for chunk in myChunkData:
-                myFileData += chunk
-                myFile.write(chunk)
-            myFile.close()
-        print "Got file named", myName, ": it is", len(myFileData), "long (units?)"
-        # so we're writing each byte as a character, we have the right data, just need to write it down AS BYTES, for some reason the string typing adds data
+
+        if not request.user.is_authenticated():
+            # TODO 1.3 security flag
+            raise Exception("Error: invalid user credentials")
+
+        bunchDir = "temp/bunches/"+myRID
+
+        uploadDest = str(os.getcwd()) + "/user_uploads/" + str(request.user.username) + "/" + datetime.date.today().isoformat()
+
+        if not os.path.exists(uploadDest):
+            os.makedirs(uploadDest)
+
+        if myType in subDirList:
+            subDest = os.path.join(uploadDest, myType)
+            if not os.path.exists(subDest):
+                os.makedirs(subDest)
+            if len(myName.split('/')) != 1 or len(myName.split('\\')) != 1 or len(myName.split('..')) != 1:
+                # TODO 1.3 security flag
+                raise Exception("Invalid file name: "+str(myName))
+            myFullPath = subDest + "/" + myName
+
+            with open(myFullPath, "wb") as myFile:
+                if os.path.exists(bunchDir):
+                    # we have actually saved bunch data for this upload, let's handle that first
+                    bunchFileNames = [f for f in os.listdir(bunchDir) if os.path.isfile(os.path.join(bunchDir, f))]
+                    bunchNums = []
+                    for bunchName in bunchFileNames:
+                        bunchNums.append(int(bunchName))
+                    bunchNums = sorted(bunchNums)
+                    for num in bunchNums:
+                        # read .bunch file named by num, write its data into myFile, then close and delete bunch
+                        numPath = bunchDir + "/" + str(num)
+                        # should double check the size of the bunchFile
+                        assert os.stat(numPath).st_size <= maxMemory, ".bunch file size error"  # TODO 1.3 better error msg
+
+                        with open(numPath, "rb") as bunchFile:
+                            myFile.write(bunchFile.read())  # assert says the file will fit in memory entirely
+                            # so we can just write directly from the input, so long as maxMemory is configured correctly
+
+                            bunchFile.close()   # more of a formality to call this but we are done, so close() it
+
+                        # delete this file
+                        os.remove(numPath)
+
+                    # done with all bunches, delete directory
+                    os.rmdir(bunchDir)
+
+                # Get currently loaded chunks from memory
+                myChunkData = chunkStorageDict[myRID]
+                myFileData = ""
+                for chunk in myChunkData:
+                    myFileData += chunk
+                    myFile.write(chunk)
+                myFile.close()
+                debug(request.user.username, "finished a file upload:", myFullPath)
+
     except Exception as e:
+        debug("fileUploadComplete:", request.user.username, "encountered error", e)
         retDict['error'] = e
     res = json.dumps(retDict)
     return HttpResponse(res, content_type='application/json')
-
-    '''
-    errorText = ""
-    RID = ''    # TODO 1.4 stopList and RID NYI, can go in with progress bar (same recoding basically)
-    # There exists a django form plugin which can accomplish the progress tracking issue (as well as allow bigger file uploads overall)
-    # TODO 1.3 Progress bar for uploading files. This part is hard to test locally, local uploads are somewhat fast
-    # TODO 1.3 progress bar should come along with chunk based uploader
-    # RID not particularly necessary for a one-way operation such as this. May be handy for logging of some description
-    # Stoplist is the main reason to use RID, assuming stop function is implemented on the front end
-    # Stopping a file upload is awkward since this is coded with only a single line technically performing the download
-    # Need to interrupt the upload somehow if stop will be relevant
-    # Further, progress bar AND stop both involve having a tighter control over this upload process, not sure if Django
-    # supports accessing upload percentages and executing commands on operations I believe it assumes are done by now
-
-    errorText = ""
-    try:
-        RID = request.POST['RID']
-    except:
-        pass
-    date = datetime.date.today().isoformat()
-    username = str(request.user.username)
-    fileForm = BulkForm(request.POST, request.FILES)    # TODO 1.3 form overhaul for chunk uploader
-    if fileForm.is_valid():
-
-        try:
-
-            uploadDest = str(os.getcwd()) + "/user_uploads/" + str(username) + "/" + date
-
-            if not os.path.exists(uploadDest):
-                os.makedirs(uploadDest)
-
-            subFilesDict = {}
-            for subDir in subDirList:
-                subFilesDict[subDir] = request.FILES.getlist(str(subDir) + "files")
-                subDest = os.path.join(uploadDest, subDir)
-                if len(subFilesDict[subDir]) > 0:
-                    if not os.path.exists(subDest):
-                        os.makedirs(subDest)
-                    try:
-                        for each_file in subFilesDict[subDir]:
-                            functions.handle_uploaded_file(each_file, subDest, each_file.name)
-                    except Exception as upfileError:
-                        errorText = "Error: " + str(upfileError)
-        except Exception as genericUploadError:
-            errorText = "Error: " + str(genericUploadError)
-    else:
-        errorText = "Error: invalid form"
-
-    return files(request, errorText)
-    '''
-
